9b5f16ce6b8234df2af3a1e79ad9cac188be71ea
Sam Joseph <tansaku@gmail.com>
2015-04-08 18:04:24 +0100

    end of stage 2

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'bcrypt-ruby'
 
 group :test do
   gem 'rspec'
--- lib/server.rb
+++ lib/server.rb
@@ -6,6 +6,7 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
 require './lib/tag'
+require './lib/user'
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
@@ -13,6 +14,9 @@ DataMapper.auto_upgrade!
 require 'sinatra'
 
 class BookmarkManager < Sinatra::Application
+
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     @links = Link.all
@@ -33,6 +37,25 @@ class BookmarkManager < Sinatra::Application
     end
     Link.create(url: url, title: title, tags: tags)
     redirect to('/')
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
+
   end
 
 end
