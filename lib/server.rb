aab37a6d1f32ab5fc10ca95011fbd29775d4fff3
Sam Joseph <tansaku@gmail.com>
2015-04-09 06:20:39 +0100

    sign in finished

--- lib/server.rb
+++ lib/server.rb
@@ -59,6 +59,22 @@ class BookmarkManager < Sinatra::Application
     end
   end
 
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    email, password = params[:email], params[:password]
+    user = User.authenticate(email, password)
+    if user
+      session[:user_id] = user.id
+      redirect to('/')
+    else
+      flash[:errors] = ['The email or password is incorrect']
+      erb :'sessions/new'
+    end
+  end
+
   helpers do
 
     def current_user
--- lib/user.rb
+++ lib/user.rb
@@ -19,4 +19,12 @@ class User
 
   validates_confirmation_of :password
 
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      return user
+    end
+    nil
+  end
+
 end
